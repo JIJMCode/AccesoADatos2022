0f69a6478e72293fb166cddeb68df00134e0447c
JIJM1819 <jimenez19454@iesmarenostrum.com>
2022-10-04 20:45:12 +0200

    Inicio

---
+++ configuracionInicial/src/main/java/com/jose/configuracionInicial/App.java
@@ -0,0 +1,13 @@
+package com.jose.configuracionInicial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
