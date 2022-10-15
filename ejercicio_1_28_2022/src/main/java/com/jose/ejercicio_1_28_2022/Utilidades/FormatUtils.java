a2759d08c749b2c35e3b88ac17b5f57853cf19cd
JIJMCode <josejimmay@gmail.com>
2022-10-15 13:11:42 +0200

    Opción menú 2 y validación coordenadas en FormarUtils

---
+++ ejercicio_1_28_2022/src/main/java/com/jose/ejercicio_1_28_2022/Utilidades/FormatUtils.java
@@ -0,0 +1,25 @@
+package com.jose.ejercicio_1_28_2022.Utilidades;
+
+public class FormatUtils {
+	public static boolean validarCoordenada(String coord, String type) {
+		boolean result = false;
+		try {
+			Double coordenada = Double.parseDouble(coord);
+			
+			if ((type.equalsIgnoreCase("latitude") && coordenada <= 90 && coordenada >= 90) ||
+				(type.equalsIgnoreCase("longitude") && coordenada <= 180 && coordenada >= -180))
+			{
+				result = true;
+			} else {
+				result = false;				
+			}		
+		} 
+		catch (Exception e) {
+			result = false;
+		}
+		finally {
+			if (!result) {System.out.println("Formato incorrecto, pruebe a introducir de nuevo");}
+			return result;
+		}	
+	}	
+}
