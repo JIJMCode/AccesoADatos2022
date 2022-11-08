0b577e7876294df8c323ec5f06b84726050abe60
JIJMCode <josejimmay@gmail.com>
2022-11-08 22:55:36 +0100

    Creación proyecto 2.15

---
+++ ejercicio_2_15_2022/src/main/java/com/jose/ejercicio_2_15_2022/App.java
@@ -0,0 +1,13 @@
+package com.jose.ejercicio_2_15_2022;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
